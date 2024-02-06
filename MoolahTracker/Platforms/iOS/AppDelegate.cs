dd7424600d115fcc32f8715e7fc14d0da10ef3ec
jacob <Jacob@projactivity.com>
2024-02-06 20:35:01 +1100

    added project folder to repo

---
+++ MoolahTracker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MoolahTracker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
